5b2139a9d73c0a3f06bc0b127f77715d970d07a3
TMowka <30312203+TMowka@users.noreply.github.com>
2018-01-04 15:31:47 +0300

    init

---
+++ sb_cookies.js
@@ -0,0 +1,80 @@
+const Nightmare = require('nightmare');
+const _ = require('lodash');
+const util = require('util');
+const config = require('./config.json');
+
+Array.prototype._cookieMapping = function (mapArray) {
+    let resultString = '';
+    for (let i = 0; i < this.length; i++) {
+        for (let j = 0; j < mapArray.length; j++) {
+            if (this[i].domain === mapArray[j].domain && mapArray[j].names.indexOf(this[i].name) > -1) {
+                resultString += `${this[i].name}=${this[i].value}; `;
+            }
+        }
+    }
+
+    return resultString;
+};
+
+Nightmare.action('clearCache',
+    function (name, options, parent, win, renderer, done) {
+        parent.respondTo('clearCache', function (done) {
+            win.webContents.session.clearCache(done);
+            done();
+
+        });
+        done();
+    },
+    function (done) {
+        this.child.call('clearCache', done);
+    });
+
+(function readArgs() {
+    let args = process.argv.splice(2);
+
+    let link = null;
+    let proxy = config.proxy;
+    switch (args.length) {
+        case 1:
+            link = args[0];
+            break;
+        case 2:
+            link = args[0];
+            proxy = args[1];
+            break;
+    }
+
+    link = 'https://store.nike.com/fr/fr_fr/product/metcon-4-id/?piid=44371&pbid=274112809';
+
+    openBrowser(link, proxy);
+})();
+
+function openBrowser(link, proxy) {
+    let checkoutBrowser = Nightmare({
+            show: config.show,
+            alwaysOnTop: config.alwaysOnTop,
+            switches: proxy ? {
+                'proxy-server': proxy.hostPort,
+                'ignore-certificate-errors': false
+            } : {},
+            webPreferences: {
+                webSecurity: false
+            }
+        }).useragent(config.userAgent)
+        .cookies.clearAll()
+        .clearCache();
+
+    setTimeout(() => {
+        checkoutBrowser
+            .authentication(proxy ? proxy.login : null, proxy ? proxy.password : null)
+            .goto(link)
+            .cookies.get()
+            .end()
+            .then((cookies) => {
+                console.log(cookies._cookieMapping(config.mapArray));
+                //console.log(util.inspect(cookies));
+            }).catch((error) => {
+                console.error(util.inspect(error));
+            });
+    }, 1000);
+}
