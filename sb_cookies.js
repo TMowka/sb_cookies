42b4c5aaec8b275d1ac3796210ce5ea07fb4bf30
TMowka <30312203+TMowka@users.noreply.github.com>
2018-01-08 13:44:11 +0300

    test

--- sb_cookies.js
+++ sb_cookies.js
@@ -3,11 +3,11 @@ const _ = require('lodash');
 const util = require('util');
 const config = require('./config.json');
 
-Array.prototype._cookieMapping = (mapArray) => {
+Array.prototype._cookieMapping = function(mapArray) {
     let resultString = '';
     for (let i = 0; i < this.length; i++) {
         for (let j = 0; j < mapArray.length; j++) {
-            if (this[i].domain === mapArray[j].domain && mapArray[j].names.indexOf(this[i].name) > -1) {
+            if (this[i].domain === mapArray[j].domain && (mapArray[j].names === '-all' || mapArray[j].names.indexOf(this[i].name) > -1)) {
                 resultString += `${this[i].name}=${this[i].value}; `;
             }
         }
@@ -44,6 +44,9 @@ Nightmare.action('clearCache',
             break;
     }
 
+    //--------TEST MODE--------
+    link = 'https://api.nike.com/deliver/available_skus/v1/?filter=productIds(c7b989ab-5965-5aaa-a66e-20145ba594bc)';
+
     openBrowser(link, proxy);
 })();
 
@@ -70,6 +73,7 @@ function openBrowser(link, proxy) {
             .end()
             .then((cookies) => {
                 console.log(cookies._cookieMapping(config.mapArray));
+                //--------TEST MODE--------
                 console.log(util.inspect(cookies));
             }).catch((error) => {
                 console.error(util.inspect(error));
